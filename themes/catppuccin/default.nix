90e949384a25ca694ed614a5fde3d863103ed5fd
Shota FUJI <pockawoooh@gmail.com>
2023-12-26 20:12:46 +0900

    Migrate bat themes option to the new attrset one
    Changed and it says will be deprecated in a future.

--- themes/catppuccin/default.nix
+++ themes/catppuccin/default.nix
@@ -349,16 +349,16 @@ in
 
       programs.bat = lib.mkIf config.programs.bat.enable {
         themes = {
-          "catppuccin-${cfg.flavor}" = builtins.readFile (
-            pkgs.fetchFromGitHub
-              {
-                owner = "catppuccin";
-                repo = "bat";
-                rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
-                sha256 = "1g2r6j33f4zys853i1c5gnwcdbwb6xv5w6pazfdslxf69904lrg9";
-              }
-            + "/Catppuccin-${cfg.flavor}.tmTheme"
-          );
+          "catppuccin-${cfg.flavor}" = {
+            src = pkgs.fetchFromGitHub {
+              owner = "catppuccin";
+              repo = "bat";
+              rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+              sha256 = "1g2r6j33f4zys853i1c5gnwcdbwb6xv5w6pazfdslxf69904lrg9";
+            };
+
+            file = "Catppuccin-${cfg.flavor}.tmTheme";
+          };
         };
 
         config = {
