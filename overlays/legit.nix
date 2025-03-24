3c319ed30a27f0fd63bf7d9b81d07c7ba4bde11e
Shota FUJI <pockawoooh@gmail.com>
2025-03-24 16:22:08 +0900

    legit: Upgrade to latest snapshot
    To use latest clean config syntax.

--- overlays/legit.nix
+++ overlays/legit.nix
@@ -4,8 +4,8 @@ final: prev:
     src = prev.fetchFromGitHub {
       owner = "pocka";
       repo = "legit";
-      rev = "bc147a9425e6265adca2672103c0d0b0dfcd735d";
-      hash = "sha256-We3ceKWo9viSfM9C/l7CvKiwfGf8bbKvH7M6M0xU1Cg=";
+      rev = "31530491fbc221dec93dc9692abe2d5fcfcc67ea";
+      hash = "sha256-lx4ShOkAbDJ04cVhVHDunrpX83HSYMlq9NGWRs0fuDs=";
     };
 
     vendorHash = "sha256-QxkMxO8uzBCC3oMSWjdVsbR2cluYMx5OOKTgaNOLHxc=";
