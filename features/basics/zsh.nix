13d7c9a548cf6803afff205880486b1db322e8b0
Shota FUJI <pockawoooh@gmail.com>
2025-03-30 07:47:10 +0000

    zsh: Prevent "dpipe" from appearing into completion list
    I don't know since when but today I hit "dp<Enter>" to complete to "dprint",
    "dpipe" stopped the workflow by popping completion list. I've never
    installed the program and I have no idea which tool is to blam, so
    hide that program from completion list.

--- features/basics/zsh.nix
+++ features/basics/zsh.nix
@@ -105,6 +105,8 @@ in
           zstyle ":vcs_info:*" formats "${vcs.info}''${CH_BRANCH} %b%c%u${text}"
           zstyle ":vcs_info:*" actionformats "[%b|%a]"
 
+          zstyle ":completion:*" ignored-patterns "dpipe"
+
           function zle-line-init zle-keymap-select {
             case $KEYMAP in
               vicmd)
