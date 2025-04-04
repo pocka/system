d5ddfd9325a4185601c5b8ecc56d5e140b299666
Shota FUJI <pockawoooh@gmail.com>
2025-04-04 01:26:10 +0000

    fcitx5: Use Wayland frontend
    Without this option enabled, home-manager sets legacy environment
    variables to start up script, which triggers warning notification from
    fcitx.

--- features/wayland-de/fcitx5.nix
+++ features/wayland-de/fcitx5.nix
@@ -13,13 +13,15 @@ in
     i18n.inputMethod = {
       enabled = "fcitx5";
 
-      fcitx5.addons = [
-        pkgs.fcitx5-mozc
-        pkgs.fcitx5-gtk
-        pkgs.libsForQt5.fcitx5-qt
-      ];
+      fcitx5 = {
+        waylandFrontend = true;
+
+        addons = [
+          pkgs.fcitx5-mozc
+          pkgs.fcitx5-gtk
+          pkgs.libsForQt5.fcitx5-qt
+        ];
+      };
     };
   };
 }
-
-
