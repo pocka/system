763d59af98edd8403970fa7e420560ff35a6ba83
Shota FUJI <pockawoooh@gmail.com>
2025-07-20 09:05:39 +0900

    Format Nix files with latest nixfmt

--- features/scm/default.nix
+++ features/scm/default.nix
@@ -78,7 +78,8 @@ in
           init = {
             defaultBranch = "master";
           };
-        } // difftasticConfig;
+        }
+        // difftasticConfig;
 
         ignores =
           let
